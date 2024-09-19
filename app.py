fd57e9f8cb992bda82871729d145073c979fc3d9
Focux45 <134445296+Focux45@users.noreply.github.com>
2024-09-19 07:32:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def on_message(client, userdata, message):
 
 broker="broker.mqttdashboard.com"
 port=1883
-client1= paho.Client("tacobell")
+client1= paho.Client("control_juan")
 client1.on_message = on_message
 
 
